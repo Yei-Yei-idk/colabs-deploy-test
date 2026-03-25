9f231a498e93de5ed861af5815673c270ee1447b
Matt3008c <carattmatt@gmail.com>
2026-03-25 09:02:38 -0500

    Arreglo de diseño en reservas finalizadas

--- resources/views/admin/reservas/finalizadas.blade.php
+++ resources/views/admin/reservas/finalizadas.blade.php
@@ -17,18 +17,18 @@
 <!-- NAVEGACIÓN Y FILTRO DE FECHAS -->
 <nav class="navegacion-fechas">
     <a href="{{ route('admin.reservas.finalizadas', ['fecha' => $fechaAnterior]) }}" class="btn-nav">
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>
-        Día anterior
+        ⬅ Día anterior
     </a>
 
     <form method="GET" action="{{ route('admin.reservas.finalizadas') }}">
-        <input type="date" class="input-fecha" name="fecha" value="{{ $fecha->format('Y-m-d') }}" onchange="this.form.submit()">
+        <input type="date" class="input-fecha" name="fecha" value="{{ $fecha->format('Y-m-d') }}" max="{{ date('Y-m-d') }}" onchange="this.form.submit()">
     </form>
 
-    <a href="{{ route('admin.reservas.finalizadas', ['fecha' => $fechaSiguiente]) }}" class="btn-nav">
-        Día siguiente
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
-    </a>
+    @if ($fecha->lt(\Carbon\Carbon::today()))
+        <a href="{{ route('admin.reservas.finalizadas', ['fecha' => $fechaSiguiente]) }}" class="btn-nav">
+            Día siguiente ➡
+        </a>
+    @endif
 </nav>
 
 <div class="reservas-grid">
